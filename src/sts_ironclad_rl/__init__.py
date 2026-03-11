63ee98795f0526c9952034ab6cb0b019d7be3ff0
Luca Cevaer-Corey <lucacc@Lucas-MBP-2.cable.rcn.com>
2026-03-11 19:32:58 -0400

    feat: add minimal combat environment

--- src/sts_ironclad_rl/__init__.py
+++ src/sts_ironclad_rl/__init__.py
@@ -2,22 +2,30 @@
 
 from .bootstrap import ProjectInfo, get_project_info
 from .env import (
+    Action,
     CombatantState,
+    CombatEnvironment,
     CombatState,
     DrawResult,
     EncounterConfig,
+    InvalidActionError,
     PileState,
+    StepResult,
     create_initial_combat_state,
     draw_cards,
 )
 
 __all__ = [
+    "Action",
     "CombatState",
     "CombatantState",
+    "CombatEnvironment",
     "DrawResult",
     "EncounterConfig",
+    "InvalidActionError",
     "PileState",
     "ProjectInfo",
+    "StepResult",
     "create_initial_combat_state",
     "draw_cards",
     "get_project_info",
